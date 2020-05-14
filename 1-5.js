4d748f920e0f61b8ed202d6dcdb51713e79b3206
kuki <bkko@outlook.kr>
2020-05-14 22:55:49 +0900

    logic improved: checkRemove only once

--- 1-5.js
+++ 1-5.js
@@ -3,27 +3,29 @@ var isSimilar = function(strA, strB){
 
     // O(N)    
     var checkRemove = function(first, second){
-        if (first.length !== second.length -1 ){
-            return false;
-        } else {
-            var found = false; //more than 2 is wrong
-            var fp = 0; //first pointer
-            var sp = 0; //second pointer
-            while ( fp < first.length ){
-                if (first[fp] !== second[sp]){
-                    if (found) {
-                        return false;
-                    } else {
-                        found = true;
-                        sp++;
-                    }
+        if (first.length !== second.length -1){
+            [first, second] = [second, first] // swap longer one to first and check once more
+            if ( first.length !== second.length -1 ){
+                return false;
+            }
+        } 
+        var found = false; //more than 2 is wrong
+        var fp = 0; //first pointer
+        var sp = 0; //second pointer
+        while ( fp < first.length ){
+            if (first[fp] !== second[sp]){
+                if (found) {
+                    return false;
                 } else {
-                    fp++;
+                    found = true;
                     sp++;
                 }
-            } 
-            return true;
-        }
+            } else {
+                fp++;
+                sp++;
+            }
+        } 
+        return true;
     };
 
     var checkDiff = function(first, second){
@@ -48,7 +50,7 @@ var isSimilar = function(strA, strB){
         }
     };
 
-    return checkRemove(strA, strB) || checkRemove(strB, strA) || checkDiff(strA, strB);
+    return checkRemove(strA, strB) || checkDiff(strA, strB);
 };
 
-console.log(isSimilar('ale', 'pale'));
+console.log(isSimilar('pale', 'bake'));
